adbe44efb60b044e18657e8f815d216d2f3f6f11
Saqib Ur Rehman <saqiburrehman75@gmail.com>
2025-06-18 14:35:26 +0500

    Updating Profile Pic using cloudinary in mongoDB and showing in frontend by getting url from cloudinary also set upload limit to 10mb

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,8 +13,8 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -63,4 +63,18 @@ export const useAuthStore = create((set) => ({
       set({ isLoggingIn: false });
     }
   },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("update profile error:", error);
+      toast.error(error?.response?.data?.message || "Profile update failed");
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
 }));
