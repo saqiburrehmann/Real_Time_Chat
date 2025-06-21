6368c5b553e662efe75593e9402455754435c029
Saqib Ur Rehman <saqiburrehman75@gmail.com>
2025-06-21 17:01:19 +0500

    Setup for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,11 +8,13 @@ import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 connectDB();
 
 // const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -26,6 +28,14 @@ app.use(
 
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
